4ca9c01ce8650d5124faee998218238eb350f724
Kieran Hayes <kchayes@uci.edu>
2019-03-13 18:48:28 -0700

    customize admin page with css and html. add display features

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,24 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    #fields that are displayed on the listings page table
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+
+    #fields that will link
+    list_display_links = ('id', 'title')
+
+    #include filters
+    list_filter = ('realtor',)
+
+    #change publish field from the table
+    list_editable = ('is_published',)
+
+    #fields that can be used to filter database
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+
+    #number of listings to display before creating pagination
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
