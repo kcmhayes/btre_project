a207bcbfb8d2d41aaf490f47fa37ac5eb3e4ac3e
Kieran Hayes <kchayes@uci.edu>
2019-03-14 17:59:42 -0700

    dynamic content for home page complete. Dynamic content for about page in progress

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 # Create your views here.
 
 def index(request):
     '''
     Render a template for a response
     '''
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
+    
+
+    context = {
+        'listings' : listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
+    realtors = Realtor.object.order_by("-hire_date")
+
+    mvp_realtors = Realtor.object.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors,
+    }
+
     return render(request, 'pages/about.html')
