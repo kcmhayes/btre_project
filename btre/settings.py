741080656c219471275f1a5b35f0ea0f85073d3a
Kieran Hayes <kcmhayes@gmail.com>
2019-03-25 14:51:29 -0700

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kcmhayes@gmail.com'
 EMAIL_HOST_PASSWORD = 'Cleary95'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
